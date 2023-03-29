8668f594d555da976c3c84d39e4afec9ab637f6c
prerakkumarpatel <prerakkumar.patel@dcmail.ca>
2023-03-29 03:27:03 -0400

    Ai Bidding function updated for suits   Bidding function added - testing require

--- AI.cs
+++ AI.cs
@@ -5,36 +5,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media.Animation;
+using static Cards.Enums;
 
 namespace Tarneeb_Card_Game
 {
     public class AI
     {
-        public int AIBid(String gamemode,int currentbid, List<Card> playersCards)
+        static int? PlaceBid(List<Card> hand, int currentBid, string gameMode)
         {
-            if(gamemode== "Easy")
+            // Count the number of cards in each suit
+            Dictionary<Suit, int> suitCounts = new Dictionary<Suit, int>();
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
             {
-                Random random = new Random();
-                int bid = random.Next(currentbid, 13);
+                suitCounts[suit] = 0;
+            }
 
-                Random rndm = new Random();
-                int dobid = rndm.Next(0, 1);
-                if(dobid==0) {
-                    return 0;
-                }
-                else
+            foreach (Card card in hand)
+            {
+                if (card.isFaceUp)
                 {
-                    return bid;
+                    suitCounts[card.Suit]++;
                 }
+            }
 
-                //Random random = new Random(currentbid,);
+            // Find the suit with the maximum number of cards
+            Suit maxSuit = Suit.Club;
+            int maxCount = 0;
+            foreach (KeyValuePair<Suit, int> pair in suitCounts)
+            {
+                if (pair.Value > maxCount)
+                {
+                    maxSuit = pair.Key;
+                    maxCount = pair.Value;
+                }
+            }
 
+            // Determine the bid based on the game mode and the maximum suit
+            int bid = currentBid;
+            switch (gameMode)
+            {
+                case "Easy":
+                    bid = Math.Max(bid, maxCount + 6);
+                    break;
+                case "Medium":
+                    bid = Math.Max(bid, maxCount + 7);
+                    break;
+                case "Hard":
+                    bid = Math.Max(bid, maxCount + 8);
+                    break;
+            }
+
+            // Make sure the bid is between 7 and 13, or return null if not
+            if (bid >= 7 && bid <= 13)
+            {
+                return bid;
             }
             else
             {
-                return 0;
+                return null;
             }
         }
+
+
+
     }
-    
+
 }
