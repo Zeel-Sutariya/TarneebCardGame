c8c57592f40bc4e7aaf46665595f2d58d89fbc9a
prerakkumarpatel <prerakkumar.patel@dcmail.ca>
2023-04-20 12:44:51 -0400

    ai refining

--- AI.cs
+++ AI.cs
@@ -21,7 +21,7 @@ namespace Tarneeb_Card_Game
         /// <param name="currentBid">highest bid that is placed till now</param>
         /// <param name="gameMode">mode of game </param>
         /// <returns>gives bid value in int from  current bid +1 and 14 for passing the bid </returns>
-        public static int PlaceBid(List<Card> hand, int currentBid, string gameMode)
+        public static int PlaceBid(List<Card> hand, int currentBid, string gameMode="Easy")
         {
             // Count the number of cards in each suit
             Dictionary<Suit, int> suitCounts = new Dictionary<Suit, int>();
@@ -73,24 +73,24 @@ namespace Tarneeb_Card_Game
 
                     Random random = new Random();
                     int randomNumber = random.Next(1, 101);
-                    if (randomNumber < 30)
+                    if (randomNumber < 20)
                     {
                         if (maxCount > 5) //best case if 8 or more card from trump suit
                         {
                             bid = random.Next(9, 14);
                         }
                         else
-                        if (HighValueCard > 5) //best case if 8 or more card from trump suit
+                        if (HighValueCard > 5)
                         {
-                            bid = random.Next(8, 10);
-                            if (bid == 9)
+                            bid = random.Next(8, 11);
+                            if (bid == 8)
                                 bid = 14;
                         }
                         else
                         if ((maxCount < 6) && (HighValueCard < 6))//best case if 8 or more card from trump suit
                         {
                             bid = random.Next(7, 9);
-                            if (bid == 8)
+                            if (bid == 7)
                                 bid = 14;
                         }
 
@@ -108,24 +108,24 @@ namespace Tarneeb_Card_Game
 
                     random = new Random();
                     randomNumber = random.Next(1, 101);
-                    if (randomNumber < 88)
+                    if (randomNumber < 77)
                     {
-                        if (maxCount > 6) //best case if 8 or more card from trump suit
+                        if (maxCount > 8) //best case if 8 or more card from trump suit
                         {
                             bid = random.Next(9, 14);
                         }
                         else
-                        if (HighValueCard > 6) //best case if 8 or more card from trump suit
+                        if (HighValueCard > 4) //best case if 8 or more card from trump suit
                         {
                             bid = random.Next(8, 10);
-                            if (bid == 9)
+                            if (bid == 8)
                                 bid = 14;
                         }
                         else
                         if ((maxCount < 6) && (HighValueCard < 6))//best case if 8 or more card from trump suit
                         {
                             bid = random.Next(7, 9);
-                            if (bid == 8)
+                            if (bid == 7)
                                 bid = 14;
                         }
 
@@ -146,22 +146,22 @@ namespace Tarneeb_Card_Game
                     randomNumber = random.Next(1, 101);
                     if (randomNumber < 98)
                     {
-                        if (maxCount > 4) //best case if 8 or more card from trump suit
+                        if (maxCount > 10) //best case if 8 or more card from trump suit
                         {
-                            bid = random.Next(9, 14);
+                            bid = random.Next(10, 14);
                         }
                         else
-                        if (HighValueCard > 4) //best case if 8 or more card from trump suit
+                        if (HighValueCard > 7) //best case if 8 or more card from trump suit
                         {
                             bid = random.Next(8, 10);
-                            if (bid == 9)
+                            if (bid == 8)
                                 bid = 14;
                         }
                         else
-                        if ((maxCount < 5) && (HighValueCard < 5))//best case if 8 or more card from trump suit
+                        if ((maxCount < 6) && (HighValueCard < 6))//best case if 8 or more card from trump suit
                         {
                             bid = random.Next(7, 9);
-                            if (bid == 8)
+                            if (bid == 7)
                                 bid = 14;
                         }
 
@@ -267,7 +267,7 @@ namespace Tarneeb_Card_Game
         /// <param name="gameMode">current game mode</param>
         /// <returns></returns>
         public static string PlayCard(List<Card> playersCard, string roundSuit, List<Card> roundCards, int inttrumpSuit, string gameMode)
-        { 
+        {
             string trumpSuit = "";
             if (inttrumpSuit == 1)
             {
@@ -311,63 +311,206 @@ namespace Tarneeb_Card_Game
 
             }
 
-
-            // Check if we have a card in the suit led
-            if (!string.IsNullOrEmpty(roundSuit) && (suitCounts[Enum.Parse<Suit>(roundSuit)] > 0))
+            if (gameMode == "Hard")
             {
-                List<Card> suitCards = playersCard.Where(c => c.Suit.ToString() == roundSuit).ToList();
-                if (suitCards.Count > 0)
+                // Check if we have a card in the suit led
+                if (!string.IsNullOrEmpty(roundSuit) && (suitCounts[Enum.Parse<Suit>(roundSuit)] > 0))
                 {
-                    highestCard = suitCards.OrderByDescending(c => c.CardNumber).First();
-
-                    lowestCard = suitCards.OrderByDescending(c => c.CardNumber).Last();
-
-                    if (highestCard >= roundHighestCard)
+                    List<Card> suitCards = playersCard.Where(c => c.Suit.ToString() == roundSuit).ToList();
+                    if (suitCards.Count > 0)
                     {
+                        highestCard = suitCards.OrderByDescending(c => c.CardNumber).First();
 
+                        lowestCard = suitCards.OrderByDescending(c => c.CardNumber).Last();
+
+                        if (highestCard >= roundHighestCard)
+                        {
+
+                            chosenCard = highestCard;
+                        }
+
+                        chosenCard = lowestCard;
+                    }
+                }
+                else
+
+                // Check if we have a trump card
+                if (!string.IsNullOrEmpty(trumpSuit) && (suitCounts[Enum.Parse<Suit>(trumpSuit)] > 0))
+
+                {
+                    List<Card> trumpCards = playersCard.Where(c => c.Suit.ToString() == trumpSuit).ToList();
+                    if (trumpCards.Count > 0)
+                    {
+                        highestCard = trumpCards.OrderByDescending(c => c.CardNumber).Last();
                         chosenCard = highestCard;
                     }
-
-                    chosenCard = lowestCard;
                 }
-            } else
+                else
 
-            // Check if we have a trump card
-            if (!string.IsNullOrEmpty(trumpSuit) && (suitCounts[Enum.Parse<Suit>(trumpSuit)] > 0))
 
-            {
-                List<Card> trumpCards = playersCard.Where(c => c.Suit.ToString() == trumpSuit).ToList();
-                if (trumpCards.Count > 0)
+                // If we still haven't found a card, play the lowest-ranking card
+
                 {
-                    highestCard = trumpCards.OrderByDescending(c => c.CardNumber).Last();
+                    highestCard = playersCard.OrderByDescending(c => c.CardNumber).Last();
                     chosenCard = highestCard;
                 }
-            } else
 
 
-            // If we still haven't found a card, play the lowest-ranking card
+                // Adjust strategy if necessary based on the highest card played so far
+                //if ( roundCard.Count > 0)
+                //{
+                //    Card highestPlayedCard = roundCard.OrderByDescending(c => c.CardNumber).First();
+                //    if (highestPlayedCard.CardNumber < highestCard.CardNumber)
+                //    {
+                //        chosenCard = playersCard.OrderBy(c => c.CardNumber).First();
+                //    }
+                //}
 
+                // Remove the chosen card from our hand and return it
+                playersCard.Remove(chosenCard);
+                string returnCard = chosenCard.Suit.ToString() + chosenCard.CardNumber.ToString();
+                return returnCard;
+            }else
+
+
+
+            if (gameMode == "Medium")
             {
+                // Check if we have a card in the suit led
+                if (!string.IsNullOrEmpty(roundSuit) && (suitCounts[Enum.Parse<Suit>(roundSuit)] > 0))
+                {
+                    List<Card> suitCards = playersCard.Where(c => c.Suit.ToString() == roundSuit).ToList();
+                    if (suitCards.Count > 0)
+                    {
+                        highestCard = suitCards.OrderByDescending(c => c.CardNumber).First();
+
+                        lowestCard = suitCards.OrderByDescending(c => c.CardNumber).Last();
+
+                        if (highestCard >= roundHighestCard)
+                        {
+
+                            chosenCard = highestCard;
+                        }
+
+                        chosenCard = lowestCard;
+                    }
+                }
+                else
+
+                // Check if we have a trump card
+                if (!string.IsNullOrEmpty(trumpSuit) && (suitCounts[Enum.Parse<Suit>(trumpSuit)] > 0))
+
+                {
+                    List<Card> trumpCards = playersCard.Where(c => c.Suit.ToString() == trumpSuit).ToList();
+                    if (trumpCards.Count > 0)
+                    {
+                        highestCard = trumpCards.OrderByDescending(c => c.CardNumber).Last();
+                        chosenCard = highestCard;
+                    }
+                }
+                else
+
+
+                // If we still haven't found a card, play the lowest-ranking card
+
+                {
+                    highestCard = playersCard.OrderByDescending(c => c.CardNumber).Last();
+                    chosenCard = highestCard;
+                }
+
+
+                // Adjust strategy if necessary based on the highest card played so far
+                //if ( roundCard.Count > 0)
+                //{
+                //    Card highestPlayedCard = roundCard.OrderByDescending(c => c.CardNumber).First();
+                //    if (highestPlayedCard.CardNumber < highestCard.CardNumber)
+                //    {
+                //        chosenCard = playersCard.OrderBy(c => c.CardNumber).First();
+                //    }
+                //}
+
+                // Remove the chosen card from our hand and return it
+                playersCard.Remove(chosenCard);
+                string returnCard = chosenCard.Suit.ToString() + chosenCard.CardNumber.ToString();
+                return returnCard;
+
+            }else
+            if (gameMode == "Easy")
+            {
+                // Check if we have a card in the suit led
+                if (!string.IsNullOrEmpty(roundSuit) && (suitCounts[Enum.Parse<Suit>(roundSuit)] > 0))
+                {
+                    List<Card> suitCards = playersCard.Where(c => c.Suit.ToString() == roundSuit).ToList();
+                    if (suitCards.Count > 0)
+                    {
+                        highestCard = suitCards.Last();
+
+                        lowestCard = suitCards.First();
+
+                        if (highestCard >= roundHighestCard)
+                        {
+
+                            chosenCard = highestCard;
+                        }
+
+                        chosenCard = lowestCard;
+                    }
+                }
+                else
+
+                // Check if we have a trump card
+                if (!string.IsNullOrEmpty(trumpSuit) && (suitCounts[Enum.Parse<Suit>(trumpSuit)] > 0))
+
+                {
+                    List<Card> trumpCards = playersCard.Where(c => c.Suit.ToString() == trumpSuit).ToList();
+                    if (trumpCards.Count > 0)
+                    {
+                        highestCard = trumpCards.Last();
+                        chosenCard = highestCard;
+                    }
+                }
+                else
+
+
+                // If we still haven't found a card, play the lowest-ranking card
+
+                {
+                    highestCard = playersCard.OrderByDescending(c => c.CardNumber).Last();
+                    chosenCard = highestCard;
+                }
+
+
+                // Adjust strategy if necessary based on the highest card played so far
+                //if ( roundCard.Count > 0)
+                //{
+                //    Card highestPlayedCard = roundCard.OrderByDescending(c => c.CardNumber).First();
+                //    if (highestPlayedCard.CardNumber < highestCard.CardNumber)
+                //    {
+                //        chosenCard = playersCard.OrderBy(c => c.CardNumber).First();
+                //    }
+                //}
+
+                // Remove the chosen card from our hand and return it
+                playersCard.Remove(chosenCard);
+                string returnCard = chosenCard.Suit.ToString() + chosenCard.CardNumber.ToString();
+                return returnCard;
+
+
+            }
+            else
+            {
+
                 highestCard = playersCard.OrderByDescending(c => c.CardNumber).Last();
                 chosenCard = highestCard;
-            } 
-            
+                playersCard.Remove(chosenCard);
+                string returnCard = chosenCard.Suit.ToString() + chosenCard.CardNumber.ToString();
+                return returnCard;
 
-            // Adjust strategy if necessary based on the highest card played so far
-            //if ( roundCard.Count > 0)
-            //{
-            //    Card highestPlayedCard = roundCard.OrderByDescending(c => c.CardNumber).First();
-            //    if (highestPlayedCard.CardNumber < highestCard.CardNumber)
-            //    {
-            //        chosenCard = playersCard.OrderBy(c => c.CardNumber).First();
-            //    }
-            //}
+            }
 
-            // Remove the chosen card from our hand and return it
-            playersCard.Remove(chosenCard);
-            string returnCard = chosenCard.Suit.ToString() + chosenCard.CardNumber.ToString();
-            return returnCard;
+
+
         }
+
     }
-    
 }
